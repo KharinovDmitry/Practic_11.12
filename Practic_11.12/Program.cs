721dfe1e894d81a1b00ebe7a11efddef037f726b
Kharinov Dmitry <web-giorno@mail.ru>
2022-11-12 20:00:52 +0500

    Gap

--- Practic_11.12/Program.cs
+++ Practic_11.12/Program.cs
@@ -4,10 +4,21 @@
     {
         static void Main(string[] args)
         {
+
             Contry[] input = Parser.Parse("./contrey.txt");
             input = Contry.SortByPoplation(input);
             string nameFile = Console.ReadLine();
             FileWriter.WriteConties(nameFile, input);
+
+            string letterString;
+            char letter;
+            do
+            {
+                letterString = Console.ReadLine();
+                letter = letterString[0];
+            } while (letterString.Length != 1);
+
+            Contry[] contriesLetter = Contry.GetContriesByLetter(input, letter);
         }
     }
 }
