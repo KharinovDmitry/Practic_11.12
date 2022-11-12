5670036fe0312d69cf271524d5d850a844c69c91
Kharinov Dmitry <web-giorno@mail.ru>
2022-11-12 19:02:40 +0500

    Class Contry

---
+++ Practic_11.12/Contry.cs
@@ -0,0 +1,11 @@
+﻿namespace Practic_11._12
+{
+    internal class Contry
+    {
+        public string Name;
+        public string Capital;
+        public int Squre;
+        public int Population;
+        public string Continent;
+    }
+}
